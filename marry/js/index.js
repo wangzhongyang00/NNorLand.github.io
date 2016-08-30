e8539a4cdb41fccc1a35d1f5c084b667a67a6887
liujiaan <hooray_K@163.com>
2016-08-30 17:10:30 +0800

    blog update

---
+++ marry/js/index.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+    // 调整音量
+    var audio = document.getElementById("bg-player");
+    audio.addEventListener('canplaythrough', function () {
+        this.volume = 0.05;
+    }, false);
+    var audioInterval = (function () {
+        var volume = audio.volume;
+        if (!volume) {
+            return;
+        }
+
+        if (volume >= 0.5) {
+            return;
+        }
+        if (volume) {
+            audio.volume += 0.05;
+        }
+    }, 1000);
+});
